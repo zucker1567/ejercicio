e01badd66fd8f2ecbf4e0c24679841253c63f634
zucker1567 <johan.romo.2000@gmail.com>
2025-07-21 15:53:07 -0400

    tailwinds instalado

---
+++ main.js
@@ -0,0 +1,25 @@
+const productos = [
+  { nombre: "Laptop Lenovo", categoria: "Tecnología", precio: 1500 },
+  { nombre: "Celular Samsung", categoria: "Tecnología", precio: 1200 },
+  { nombre: "Auriculares JBL", categoria: "Tecnología", precio: 300 },
+  { nombre: "Mouse Logitech", categoria: "Tecnología", precio: 150 },
+  { nombre: "Teclado Redragon", categoria: "Tecnología", precio: 200 },
+
+  { nombre: "Sofá 3 plazas", categoria: "Hogar", precio: 850 },
+  { nombre: "Mesa de comedor", categoria: "Hogar", precio: 400 },
+  { nombre: "Lámpara de pie", categoria: "Hogar", precio: 90 },
+  { nombre: "Alfombra persa", categoria: "Hogar", precio: 300 },
+  { nombre: "Silla ergonómica", categoria: "Hogar", precio: 220 },
+
+  { nombre: "Remera blanca", categoria: "Ropa", precio: 50 },
+  { nombre: "Pantalón jeans", categoria: "Ropa", precio: 120 },
+  { nombre: "Campera de cuero", categoria: "Ropa", precio: 350 },
+  { nombre: "Zapatillas Nike", categoria: "Ropa", precio: 250 },
+  { nombre: "Gorra negra", categoria: "Ropa", precio: 40 },
+
+  { nombre: "Martillo", categoria: "Ferretería", precio: 60 },
+  { nombre: "Taladro eléctrico", categoria: "Ferretería", precio: 400 },
+  { nombre: "Caja de herramientas", categoria: "Ferretería", precio: 220 },
+  { nombre: "Destornillador", categoria: "Ferretería", precio: 30 },
+  { nombre: "Cinta métrica", categoria: "Ferretería", precio: 25 },
+];
