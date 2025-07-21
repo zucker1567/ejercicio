09bc084392fee146a7f1bd84b36d48cfdd189efa
Steven <stevenorozco2021@gmail.com>
2025-07-21 16:25:41 -0400

    ImagenesProducto

---
+++ main.js
@@ -0,0 +1,27 @@
+let productoImagen=[
+
+    {Laptoplenovo:"https://cdn.pacifiko.com/image/cache/catalog/p/ZDkzNjc0MT_1-1000x1000.png"},
+    {CelularSamsug:"https://i.ebayimg.com/thumbs/images/g/9roAAeSw6f5oekfj/s-l1200.jpg"},
+    {AudicularesJBL:"https://covercompany.com.uy/cdn/shop/files/2-4063_10760_1.jpg?v=1730480785"},
+    {MauseLogitech:"https://casacuesta.com/media/catalog/product/cache/fde49a4ea9a339628caa0bc56aea00ff/3/3/3339039-1.jpg"},
+    {TecladoRedragon:""},
+
+    {SofaPlazas:"https://korsaka.com/wp-content/uploads/2023/12/Teclado-Redragon-60-inalambrico-Bluetooth-Fizz-Pro-K616.png"},
+    {MesaComedor:"https://m.media-amazon.com/images/I/81dTQi3kxOL._UF894,1000_QL80_.jpg"},
+    {LamparaPie:"https://www.tiendasmontevideo.com.uy/wp-content/uploads/2025/02/24600897810001-1.jpg"},
+    {Alfombrapersa:"https://m.media-amazon.com/images/I/81akfzMm06L._UF894,1000_QL80_.jpg"},
+    {SillaErgonomica:"https://ouomx.com/cdn/shop/files/1_Amazon_26-02-25_v1.jpg?v=1741986511"},
+
+    {RemeraBlancla:"https://img.freepik.com/foto-gratis/retrato-guapo-sonriente-elegante-hipster-lambersexual-modelman-vestido-blanco-camiseta-vaqueros-moda-macho-posar-cerca-pared-estudio-aislado_158538-22615.jpg?semt=ais_hybrid&w=740"},
+    {PantalonJeans:"https://caterpillargt.com/cdn/shop/files/30075916_9f5d7e9e-75ec-4363-b797-988bffc24dd4_1024x1024.jpg?v=1751437370"},
+    {CamperaCuero:"https://www.digitalsport.com.ar/files/products/62b5ebed9406b-593466-500x500.jpg"},
+    {ZapatillasNike:"https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw9d732159/products/NIDX3705-001/NIDX3705-001-6.JPG"},
+    {GorraNegra:"https://merxstore.mx/cdn/shop/files/76415-R01.jpg?v=1724877767"},
+    
+    {Martillo:"https://media.kleintools.io/images/original/klein/h80816.jpg"},
+    {TaladroElectrico:"https://kywiec.vtexassets.com/arquivos/ids/172947-800-auto?v=638388536202070000&width=800&height=auto&aspect=true"},
+    {CajaHerramienta:"https://www.mikels.com.mx/media/catalog/product/cache/95aae7ce13a353d20b736b25a0c71a20/c/h/ch-88_2.jpg"},
+    {Destornillador:"https://yoclaudiomercantil.com.do/wp-content/uploads/2022/08/destornillador-estria-3-8-x-8-ingco-yoclaudiomercantil.jpg"},
+    {CintaMetrica:"https://cafesacomercial.com/wp-content/uploads/2023/03/cinta-metrica-5m-tolsen.jpg"},
+
+]
